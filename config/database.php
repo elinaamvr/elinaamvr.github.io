d0793010ae25359911ee6e33c4d56ee26f7880e2
elinaamvr <73417669+elinaamvr@users.noreply.github.com>
2020-11-23 11:43:59 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,4 +1,28 @@
 <?php
 
+class Database{
+  
+    //Database credentials
+    private $host = "localhost";
+    private $db_name = "webeng20g2";
+    private $username = "webeng20g2";
+    private $password = "2021g2";
+    public $conn;
+  
+    // get the database connection
+    public function getConnection(){
+  
+        $this->conn = null;
+  
+        try{
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+           
+        }catch(PDOException $e){
+            echo "Connection error: " . $e->getMessage();
+        }
+  
+        return $this->conn;
+    }
+}
 
 ?>
